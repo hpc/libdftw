08f0d5be6a47a81680378f80b049f6e74e4bd24c
jonb <jonb@lanl.gov>
2012-03-18 16:29:34 -0600

    Addition of an example.

---
+++ example/libdftw_example.c
@@ -0,0 +1,32 @@
+/*
+ * An example of using libdftw.
+ */
+
+#include "libdftw_example.h"
+
+static int
+display_info(const char *fpath, const struct stat *sb, int tflag) {
+    fprintf(stdout, "Walking: `%s'\n", fpath);
+
+    printf("%-3s %7jd   %-40s\n",
+        (tflag == FTW_D) ?   "d"   : (tflag == FTW_DNR) ? "dnr" :
+        (tflag == FTW_NS) ?  "ns"  : (tflag == FTW_F) ?   "f" : "???",
+        (intmax_t) sb->st_size, fpath);
+
+    return 0;           /* To tell nftw() to continue */
+}
+
+int main(int argc, char **argv)
+{
+    if(argc != 2) {
+        fprintf(stderr, "Usage: %s <path>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "Walking with root as: `%s'\n", argv[1]);
+    dftw(argv[1], display_info);
+
+    exit(EXIT_SUCCESS);
+}
+
+/* EOF */
---
+++ example/libdftw_example.h
@@ -0,0 +1,14 @@
+#ifndef __LIBDFTW_EXAMPLE_H
+#define __LIBDFTW_EXAMPLE_H
+
+#include <libdftw.h>
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+static int
+display_info(const char *fpath, const struct stat *sb, int typeflag);
+
+#endif /* __LIBDFTW_EXAMPLE_H */
